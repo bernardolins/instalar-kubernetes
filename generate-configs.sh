4489a83edbae7d3e8ab0e77917b177b934a740a3
Bernardo Lins <bernardolins28@gmail.com>
2016-02-20 00:15:03 -0200

    Adicionando montagem de variáveis de ambiente que serão usadas para configurações

--- generate-configs.sh
+++ generate-configs.sh
@@ -24,7 +24,6 @@ while [ $# -ge 1 ]; do
 done
 
 ###### kubernetes ######
-
 version=$(cat $config | jsawk 'return this.version')
 token=$(cat $config | jsawk 'return this.token')
 
@@ -32,12 +31,74 @@ token=$(cat $config | jsawk 'return this.token')
 masterip=$(cat $config | jsawk 'return this.master.ip')
 masterhostname=$(cat $config | jsawk 'return this.master.hostname')
 masterinterface=$(cat $config | jsawk 'return this.master.interface')
+masteriphostname=$(cat $config | jsawk -n 'out (this.master.hostname + "=http://" + this.master.ip + ":2380")')
 
 ###### workers ######
-workersip=$(cat $config | jsawk 'return this.workers')
+workers=$(cat $config | jsawk 'return this.workers')
 
-workerips=$(echo $workersip | jsawk 'return this.ip')
-workerhostnames=$(echo $workersip | jsawk 'return this.hostname')
-workerinterfaces=$(echo $workersip | jsawk 'return this.interface')
+workerips=$(echo $workers | jsawk -n 'out(this.ip)')
+workerhostnames=$(echo $workers | jsawk -n 'out(this.hostname)')
+workerinterfaces=$(echo $workers | jsawk -n 'out (this.interface)')
 
-echo $workerips $workerhostnames $workerinterfaces
+workeriphostname=$(echo $workers | jsawk -n 'out (this.hostname + "=http://" + this.ip + ":2380")')
+
+function set_endpoints {
+  endpoints=""
+
+  for ip in $masterip; do
+    if [ -n "$endpoints" ]; then
+      endpoints="$endpoints,"
+    fi
+
+    address="http://$ip:2379" 
+    endpoints="$endpoints$address"
+  done
+
+  for ip in $workerips; do
+    if [ -n "$endpoints" ]; then
+      endpoints="$endpoints,"
+    fi
+
+    address="http://$ip:2379" 
+    endpoints="$endpoints$address"
+  done
+
+  echo $endpoints
+}
+
+function set_initial_cluster {
+  nodes=""
+
+  for node in $masteriphostname; do
+    if [ -n "$nodes" ]; then
+      nodes="$nodes,"
+    fi
+
+    nodes="$nodes$node"
+  done
+
+  for node in $workeriphostname; do
+    if [ -n "$nodes" ]; then
+      nodes="$nodes,"
+    fi
+
+    nodes="$nodes$node"
+  done
+
+  echo $nodes
+}
+
+###### Setting up variables ######
+
+###### Defaults ######
+POD_NETWORK=10.2.0.0/16
+SERVICE_IP_RANGE=10.3.0.0/24
+K8S_SERVICE_IP=10.3.0.1
+DNS_SERVICE_IP=10.3.0.10
+
+###### Cluster specific ######
+
+ETCD_ENDPOINTS=$(set_endpoints)
+INITIAL_CLUSTER=$(set_initial_cluster)
+
+echo $INITIAL_CLUSTER
