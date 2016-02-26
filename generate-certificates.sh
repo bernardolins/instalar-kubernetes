ae20126e170d4f9251c9ca1ae3569f13b1d4e05a
Bernardo Lins <bernardolins28@gmail.com>
2016-02-26 01:11:47 -0300

    Fazendo uma limpeza nos certificados e copiando os certificados para diretório ssl dos nodes

--- generate-certificates.sh
+++ generate-certificates.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+#### Config
 cat << EOF > $token/openssl.cnf
 [req]
 req_extensions = v3_req
@@ -19,6 +20,7 @@ IP.1 = ${K8S_SERVICE_IP}
 IP.2 = ${MASTER_HOST}
 EOF
 
+#### Root certificates
 openssl genrsa -out $token/ca-key.pem 2048
 openssl req -x509 -new -nodes -key $token/ca-key.pem -days 10000 -out $token/ca.pem -subj "/CN=kube-ca"
 
@@ -30,9 +32,17 @@ openssl genrsa -out $token/admin-key.pem 2048
 openssl req -new -key $token/admin-key.pem -out $token/admin.csr -subj "/CN=kube-admin"
 openssl x509 -req -in $token/admin.csr -CA $token/ca.pem -CAkey $token/ca-key.pem -CAcreateserial -out $token/admin.pem -days 365
 
+rm -rf $token/openssl.cnf
+rm -rf $token/*.csr
+rm -rf $token/*.srl
+
+#### Generate workers certificates
 for (( i=0; i<${#iparray[@]}; i++ )); do
 
-cat << EOF > $token/worker/${hostnamearray[$i]}/${hostnamearray[$i]}-openssl.cnf
+mkdir -p $token/worker/${hostnamearray[$i]}/ssl
+
+#### Worker config
+cat << EOF > $token/worker/${hostnamearray[$i]}/ssl/${hostnamearray[$i]}-openssl.cnf
   [req]
   req_extensions = v3_req
   distinguished_name = req_distinguished_name
@@ -45,10 +55,18 @@ cat << EOF > $token/worker/${hostnamearray[$i]}/${hostnamearray[$i]}-openssl.cnf
   IP.1 = ${iparray[$i]}
 EOF
 
-openssl genrsa -out $token/worker/${hostnamearray[$i]}/${hostnamearray[$i]}-worker-key.pem 2048
+openssl genrsa -out $token/worker/${hostnamearray[$i]}/ssl/${hostnamearray[$i]}-worker-key.pem 2048
 
-WORKER_IP=${iparray[$i]} openssl req -new -key $token/worker/${hostnamearray[$i]}/${hostnamearray[$i]}-worker-key.pem -out $token/worker/${hostnamearray[$i]}/${hostnamearray[$i]}-worker.csr -subj "/CN=${hostnamearray[$i]}" -config $token/worker/${hostnamearray[$i]}/${hostnamearray[$i]}-openssl.cnf
+WORKER_IP=${iparray[$i]} openssl req -new -key $token/worker/${hostnamearray[$i]}/ssl/${hostnamearray[$i]}-worker-key.pem -out $token/worker/${hostnamearray[$i]}/ssl/${hostnamearray[$i]}-worker.csr -subj "/CN=${hostnamearray[$i]}" -config $token/worker/${hostnamearray[$i]}/ssl/${hostnamearray[$i]}-openssl.cnf
 
-WORKER_IP=${iparray[$i]} openssl x509 -req -in $token/worker/${hostnamearray[$i]}/${hostnamearray[$i]}-worker.csr -CA $token/ca.pem -CAkey $token/ca-key.pem -CAcreateserial -out $token/worker/${hostnamearray[$i]}/${hostnamearray[$i]}-worker.pem -days 365 -extensions v3_req -extfile $token/worker/${hostnamearray[$i]}/${hostnamearray[$i]}-openssl.cnf
+WORKER_IP=${iparray[$i]} openssl x509 -req -in $token/worker/${hostnamearray[$i]}/ssl/${hostnamearray[$i]}-worker.csr -CA $token/ca.pem -CAkey $token/ca-key.pem -CAcreateserial -out $token/worker/${hostnamearray[$i]}/ssl/${hostnamearray[$i]}-worker.pem -days 365 -extensions v3_req -extfile $token/worker/${hostnamearray[$i]}/ssl/${hostnamearray[$i]}-openssl.cnf
+
+cp $token/ca.pem $token/worker/${hostnamearray[$i]}/ssl
+rm -f $token/worker/${hostnamearray[$i]}/ssl/${hostnamearray[$i]}-openssl.cnf
+rm -f $token/worker/${hostnamearray[$i]}/ssl/*.csr
 
 done
+
+#### Copy master certificates to correct place
+mkdir -p $token/master/$masterhostname/ssl
+cp $token/ca.pem $token/apiserver-key.pem $token/apiserver.pem $token/master/$masterhostname/ssl
